b254851d1977787d5bfcb95cb1885dc29b30bb75
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-10 15:40:06 +0330

    استفاده از ریداکس در ریکت

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { Container } from "react-bootstrap";
 import "./App.scss";
 
+// !redux
+import { Provider } from "react-redux";
+import store from "./redux/Store";
+
 // ?components
 import Counter from "./components/Counter";
 
 function App() {
   return (
-    <Container className="text-center">
-      <Counter />
-    </Container>
+    <Provider store={store}>
+      <Container className="text-center">
+        <Counter />
+      </Container>
+    </Provider>
   );
 }
 
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-const Counter = () => {
+// ?redux
+import { increase } from "../redux/counter/counterAction";
+import { connect } from "react-redux";
+
+const Counter = (props) => {
   return (
     <div>
-      <h1>counter</h1>
-      <Button>Increase</Button>
+      <h1>counter {props.counter}</h1>
+      <Button onClick={props.increase}>Increase</Button>
     </div>
   );
 };
 
-export default Counter;
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increase: () => dispatch(increase()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
