b58bb78660dec7f53a17deaef985067d3916758f
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-10 15:55:53 +0330

    استفاده از هوک های ریداکس و ساده تر شدن ریداکس

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,27 +3,18 @@ import { Button } from "react-bootstrap";
 
 // ?redux
 import { increase } from "../redux/counter/counterAction";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
-const Counter = (props) => {
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <h1>counter {props.counter}</h1>
-      <Button onClick={props.increase}>Increase</Button>
+      <h1>counter {counter}</h1>
+      <Button onClick={() => dispatch(increase())}>Increase</Button>
     </div>
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    counter: state.counter,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increase: () => dispatch(increase()),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter;
