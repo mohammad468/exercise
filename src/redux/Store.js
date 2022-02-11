3c008dc82191107b047eaedd8f2f653a7f904a3b
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-11 15:55:49 +0330

    Middleware & logger in redux

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,4 +1,6 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
-const store = createStore(rootReducer);
+import logger from "redux-logger";
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 export default store;
