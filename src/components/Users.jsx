47f292ebf918fd59a38a197badc9198a3599201a
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-11 18:43:51 +0330

    fetch data async in redux thunk

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from "react";
+
+// !redux
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUsers } from "../redux/users/usersAction";
 
 const Users = () => {
-    return (
-        <div>
-            Users
-        </div>
-    );
+  const dispatch = useDispatch();
+  const usersData = useSelector((state) => state.usersState);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
+  return (
+    <div>
+      <h1>usersList</h1>
+      {usersData.loading ? (
+        <h2>loading...</h2>
+      ) : usersData.error ? (
+        <p>{usersData.error}</p>
+      ) : (
+        usersData.users.map((user) => <p key={user.id}>{user.name}</p>)
+      )}
+    </div>
+  );
 };
 
 export default Users;
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,10 +3,12 @@ import { combineReducers } from "redux";
 // !reducers
 import counterReducer from "./counter/counterReducer";
 import userReducer from "./user/userReducer";
+import usersReducer from "./users/usersReducer";
 
 const rootReducer = combineReducers({
   counterState: counterReducer,
   userState: userReducer,
+  usersState: usersReducer,
 });
 
 export default rootReducer;
