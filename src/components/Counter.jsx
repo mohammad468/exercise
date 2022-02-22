4472e83013ef7adb09c37e10c1262fcb82705fa4
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-22 13:33:25 +0330

    fix center

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import ShowNumber from "./ShowNumber";
 
-
 const Counter = () => {
   return (
     <div>
-      <div className="d-flex align-items-center">
+      <div className="d-flex flex-column align-items-center justify-content-center">
         <ShowNumber />
         <Button>Increase</Button>
       </div>
