37b76566a9df67246d87219f3866488e612268e9
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-22 14:22:49 +0330

    use contexts text is ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
 import "./App.scss";
 
+// ?context
+import { useIncrease } from "./context/IncreaseContext";
+
 const App = () => {
+  const { increase, setIncrease } = useIncrease();
+
+  const clgHandler = () => {
+    console.log(increase);
+  };
+
   return (
     <div>
       <h1>hello World</h1>
+      <Button onClick={clgHandler}>clg</Button>
     </div>
   );
 };
