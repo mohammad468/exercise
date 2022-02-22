3f03102973bb3c50f0fb80e700323d8b0bedb985
mohammad mohseni <mohammad.mohseni468@gmail.com>
2022-02-22 14:13:28 +0330

    provide increase

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { IncreaseContextProvider } from "./context/IncreaseContext";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <IncreaseContextProvider>
+      <App />
+    </IncreaseContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
